d9a17191d5fe792f666ceb2379a8ac6b9da7615d
fedics Gorbachev <fedicssmen@mail.ru>
2026-06-22 22:03:12 +0400

    create: класс для юзера и подлкючения к бд

---
+++ MySpravki/DB.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySpravki
+{
+    public class DB
+    {
+        MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;user id = root;password=;Database=testPOCA");
+
+        public void openConnection()
+        {
+            connection.Open();
+        }
+        public void closeConnection()
+        {
+            connection.Close();
+        }
+
+        public MySqlConnection getConnection()
+        {
+            return connection;
+        }
+    }
+}
---
+++ MySpravki/User.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace MySpravki
+{
+    public class User
+    {
+        public int id;
+        public string login;
+        public string password;
+
+        public static string UserAuth(string Login, string Password)
+        {
+            DB db = new DB();
+            try
+            {
+                db.openConnection();
+                string query = "SELECT id FROM `user` WHERE login = @Login AND password = @Password;";
+                MySqlCommand cmd = new MySqlCommand(query, db.getConnection());
+                cmd.Parameters.AddWithValue("@Login", Login);
+                cmd.Parameters.AddWithValue("@Password", Password);
+                var res = cmd.ExecuteScalar();
+                if (res != null)
+                {
+                    return res.ToString();
+                }
+                else
+                {
+                    return null;
+                }
+
+                
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+            finally
+            {
+                db.closeConnection();
+            }
+        }
+    }
+
+    
+
+
+}
